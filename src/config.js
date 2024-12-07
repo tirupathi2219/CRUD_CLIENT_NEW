14e13da65db53b98ee14083be00831941ead8588
Tirupathi Rao <125377941+tirupathi2219@users.noreply.github.com>
2024-12-07 08:44:20 +0000

    backend-url

--- src/config.js
+++ src/config.js
@@ -1,5 +1,8 @@
 const config = {
-    BE_URL: process.env.REACT_APP_API_BASE_URL || 'http://localhost:8000'
-  };
-  
-  export default config;
+  BE_URL:
+    process.env.NODE_ENV === "development"
+      ? process.env.REACT_APP_API_BASE_URL_DEV
+      : process.env.REACT_APP_API_BASE_URL_PROD,
+};
+
+export default config;
